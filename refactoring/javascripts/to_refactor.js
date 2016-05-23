e9761185be95231f86ac29952b495fface09c3d4
Taylor Peat <taylorpeat@hotmail.com>
2016-05-23 06:35:31 -0400

    Finish refactoring

--- refactoring/javascripts/to_refactor.js
+++ refactoring/javascripts/to_refactor.js
@@ -1,41 +1,35 @@
 $(function() {
   $("nav a").on("mouseenter", function() {
-    $(this).next("ul").css({
-      display: "block"
-    });
+    $(this).next("ul").removeClass().addClass("visible");
   });
 
   $("nav").on("mouseleave", function() {
-    $(this).find("ul ul").css({
-      display: "none"
-    });
+    $(this).find("ul ul").removeClass().addClass("hidden");
   });
 
-  $(".button").on("click", function(e) {
+  $(".button, button").on("click", function(e) {
     e.preventDefault();
 
-    $(this).addClass("clicked");
-  });
-
-  $("button").on("click", function() {
     $(this).addClass("clicked");
   });
 
   $(".toggle").on("click", function(e) {
     e.preventDefault();
 
-    if ($(this).next(".accordion").hasClass("opened")) {
-      $(this).next(".accordion").removeClass("opened");
-    }
-    else {
-      $(this).next(".accordion").addClass("opened");
-    }
+    $(this).next(".accordion").toggleClass("opened");
   });
 
   $("form").on("submit", function(e) {
     e.preventDefault();
-    var cc_number = $(this).find("[type=text]").val(),
-        odd_total = 0,
+    var cc_number = $(this).find("[type=text]").val();
+        is_valid = cardCheck(cc_number) % 10 === 0;
+
+    $(this).find(".success").toggle(is_valid);
+    $(this).find(".error").toggle(!is_valid);
+  });
+
+  function cardCheck(cc_number) {
+    var odd_total = 0,
         even_total = 0;
 
     cc_number = cc_number.split("").reverse();
@@ -54,59 +48,29 @@ $(function() {
         even_total += +cc_number[i];
       }
     }
-    if ((odd_total + even_total) % 10 == 0) {
-      $(this).find(".success").show();
-      $(this).find(".error").hide();
-    }
-    else {
-      $(this).find(".error").show();
-      $(this).find(".success").hide();
-    }
-  });
+    return odd_total + even_total;
+  }
 
   $("ul a").on("click", function(e) {
     e.preventDefault();
-
-    var month = $(this).text(),
+    var birthstones = {
+          January: "garnet",
+          February: "amethyst",
+          March: "aquamarine or bloodstone",
+          April: "diamond",
+          May: "emerald",
+          June: "pearl, moonstone or alexandrite",
+          July: "ruby",
+          August: "peridot",
+          September: "sapphire",
+          October: "opal or tourmaline",
+          November: "topaz or citrine",
+          December: "turquoise, zircon, or tanzanite"
+        },
+        month = $(this).text(),
+        birthstone = birthstones[month];
         $stone = $("#birthstone");
-
-    switch (month) {
-      case "January":
-        $stone.text("Your birthstone is garnet");
-        break;
-      case "February":
-        $stone.text("Your birthstone is amethyst");
-        break;
-      case "March":
-        $stone.text("Your birthstone is aquamarine or bloodstone");
-        break;
-      case "April":
-        $stone.text("Your birthstone is diamond");
-        break;
-      case "May":
-        $stone.text("Your birthstone is emerald");
-        break;
-      case "June":
-        $stone.text("Your birthstone is pearl, moonstone, or alexandrite");
-        break;
-      case "July":
-        $stone.text("Your birthstone is ruby");
-        break;
-      case "August":
-        $stone.text("Your birthstone is peridot");
-        break;
-      case "September":
-        $stone.text("Your birthstone is sapphire");
-        break;
-      case "October":
-        $stone.text("Your birthstone is opal or tourmaline");
-        break;
-      case "November":
-        $stone.text("Your birthstone is topaz or citrine");
-        break;
-      case "December":
-        $stone.text("Your birthstone is turquoise, zircon, or tanzanite");
-        break;
-    }
+    
+    $stone.text("Your birthstone is " + birthstone);
   });
 });
